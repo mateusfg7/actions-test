51ba8833881c338ad76c80eb004265f2bc462c52
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-19 12:28:59 -0300

    COMMIT: Mon Dec 19 12:28:59 PM -03 2022

--- tools/bump-tag-version.ts
+++ tools/bump-tag-version.ts
@@ -1,8 +1,5 @@
-import isomorphicGit from "npm:isomorphic-git";
-import http from "npm:isomorphic-git/http/node/index.js";
 // @deno-types="npm:@types/semver"
 import semver from "npm:semver";
-import * as fs from "https://deno.land/std@0.170.0/node/fs.ts";
 import * as patch from "https://deno.land/std@0.170.0/node/path/mod.ts";
 import { simpleGit, SimpleGit } from 'npm:simple-git'; 
 
@@ -12,24 +9,20 @@ const dir = patch.join(Deno.cwd(), "./");
 
 const simplegit: SimpleGit = simpleGit(dir);
 
-
-async function getLastTag() {
-  const tags = await isomorphicGit.listTags({ fs, dir });
-  const sortedTags = semver.rsort(tags);
-
-  return sortedTags[0];
-}
-
-const lastTag = await getLastTag();
+const tags = await simplegit.tags();
+const lastTag = tags.latest;
 const packageVersion = packageJson.version;
 
-console.log(`Latest Tag: ${lastTag}\nPackage Version: ${packageVersion}`);
-console.log(`Package Version is greather than Latest Tag? ${semver.gt(packageVersion, lastTag)}`);
-
-if (semver.gt(packageVersion, lastTag)) {
-  console.log('Bump Tag')
-  await isomorphicGit.tag({ fs, dir, ref: packageVersion })
+if (lastTag) {
+  console.log(`Latest Tag: ${lastTag}\nPackage Version: ${packageVersion}`);
+  console.log(`Package Version is greather than Latest Tag? ${semver.gt(packageVersion, lastTag)}`);
+  
+  if (semver.gt(packageVersion, lastTag)) {
+    console.log('Bump Tag')
+    await simplegit.addTag(packageVersion)
+  }
+  
+  console.log('Pushing Tag')
+  await simplegit.pushTags()
 }
 
-console.log('Pushing Tag')
-await simplegit.pushTags()
