923fb01d89accd7cd710e3d602d7a3c8e646b8d7
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-19 12:56:04 -0300

    COMMIT: Mon Dec 19 12:56:04 PM -03 2022

--- tools/bump-tag-version.ts
+++ tools/bump-tag-version.ts
@@ -5,25 +5,34 @@ import { simpleGit, SimpleGit } from 'npm:simple-git';
 
 import packageJson from '../package.json' assert {type: 'json' }
 
+function log(text: string) {
+  console.log(`LOG | ${text}`)
+}
+
+log('Get current working directory')
 const dir = patch.join(Deno.cwd(), "./");
 
+log('Init simple-git instance')
 const simplegit: SimpleGit = simpleGit(dir);
 
+log('Get latest repository tag and package version')
 const tags = await simplegit.tags();
 const lastTag = tags.latest;
 const packageVersion = packageJson.version;
 
+log('Verify if latest tag is not undeined')
+
 if (lastTag) {
-  console.log(`Latest Tag: ${lastTag}\nPackage Version: ${packageVersion}\n`);
+  log(`Latest Tag: ${lastTag}\nPackage Version: ${packageVersion}\n`);
   
   if (semver.gt(packageVersion, lastTag)) {
-    console.log('Synchronizing tag')
+    log('Synchronizing tag')
     await simplegit.addTag(packageVersion)
 
-    console.log('Pushing tags')
+    log('Pushing tags')
     await simplegit.pushTags()
   } else {
-    console.log('Last tag and package version are synchronized')
+    log('Last tag and package version are synchronized')
   }
 }
 
