9c71c1ea06f2bd39dd6856478fdda3d1eff67a83
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-12-19 13:12:17 -0300

    COMMIT: Mon Dec 19 01:12:17 PM -03 2022

--- tools/bump-tag-version.ts
+++ tools/bump-tag-version.ts
@@ -1,28 +1,18 @@
 // @deno-types="npm:@types/semver"
 import semver from "npm:semver";
-import * as patch from "https://deno.land/std@0.170.0/path/mod.ts";
 import { simpleGit, SimpleGit } from 'npm:simple-git'; 
-
 import packageJson from '../package.json' assert {type: 'json' }
 
 function log(text: string) {
   console.log(`LOG | ${text}`)
 }
 
-log(`Working directory: ${Deno.cwd()}`)
-
-log('Init simple-git instance')
 const simplegit: SimpleGit = simpleGit(Deno.cwd());
 
-log('Get latest repository tag and package version\n')
 const tags = await simplegit.tags();
 const lastTag = tags.latest;
 const packageVersion = packageJson.version;
 
-log(`Package Version ${packageVersion}`)
-log(`Last Tag ${lastTag}\n`)
-
-log('Verify if latest tag is not undefined')
 if (lastTag) {
   if (semver.gt(packageVersion, lastTag)) {
     log('Synchronizing tag')
@@ -36,4 +26,3 @@ if (lastTag) {
 } else {
   log('There is no latest tag')
 }
-
